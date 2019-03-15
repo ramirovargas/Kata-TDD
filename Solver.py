b950426d19a24b36e77e6266f8d3753686d2847e
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-03-15 13:05:19 -0500

    Verde iteracion 3

--- Solver.py
+++ Solver.py
@@ -5,11 +5,12 @@ class Solver:
         if(len(cadena)==0):
             arr.append(0)
             arr.append(0)
+            arr.append(0)
 
         else:
             result = [x.strip() for x in cadena.split(',')]
             arr.append(len(result))
             arr.append(int(min(result)))
-
+            arr.append(int(max(result)))
         return arr
 
--- test_solver.py
+++ test_solver.py
@@ -5,10 +5,7 @@ from Solver import Solver
 class TestSolver(TestCase):
     def test_calculate_iter1(self):
         s=Solver()
-        self.assertTrue(s.calculate("")==[0,0])
+        self.assertTrue(s.calculate("")==[0,0,0])
     def test_calculate_iter2(self):
         s=Solver()
-        self.assertTrue(s.calculate("1,2,3,4")==[4,1])
-    def test_calculate_iter3(self):
-        s=Solver()
-        self.assertTrue(s.calculate("2,2,3,4,5,6,7,8")==[8,2])
+        self.assertTrue(s.calculate("2,4,5,6,7")==[5,2,7])
