b250af388df6bbcce665c4342823b6c5c085e438
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-03-15 12:39:43 -0500

    TDD1 Rojo iteracion 3 y 4

--- test_solver.py
+++ test_solver.py
@@ -8,4 +8,10 @@ class TestSolver(TestCase):
         self.assertTrue(s.calculate("")==[0])
     def test_calculate_iter2(self):
         s=Solver()
-        self.assertTrue(s.calculate("1")==[1])
+        self.assertTrue(s.calculate("4")==[1])
+    def test_calculate_iter3(self):
+        s=Solver()
+        self.assertTrue(s.calculate("4,3")==[2])
+    def test_calculate_iter4(self):
+        s=Solver()
+        self.assertTrue(s.calculate("4,3,6,7,8,9")==[6])
