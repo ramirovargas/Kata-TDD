e219c695260dc27f98196ca772102ab3d6609dad
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-03-15 12:59:14 -0500

    Rojo iteracion 2

--- Solver.py
+++ Solver.py
@@ -4,9 +4,12 @@ class Solver:
         arr=[]
         if(len(cadena)==0):
             arr.append(0)
+            arr.append(0)
 
         else:
             result = [x.strip() for x in cadena.split(',')]
             arr.append(len(result))
+            arr.append(int(min(result)))
 
         return arr
+
--- test_solver.py
+++ test_solver.py
@@ -5,13 +5,7 @@ from Solver import Solver
 class TestSolver(TestCase):
     def test_calculate_iter1(self):
         s=Solver()
-        self.assertTrue(s.calculate("")==[0])
-    def test_calculate_iter2(self):
+        self.assertTrue(s.calculate("")==[0,0])
+    def test_calculate_iter1(self):
         s=Solver()
-        self.assertTrue(s.calculate("4")==[1])
-    def test_calculate_iter3(self):
-        s=Solver()
-        self.assertTrue(s.calculate("4,3")==[2])
-    def test_calculate_iter4(self):
-        s=Solver()
-        self.assertTrue(s.calculate("4,3,6,7,8,9")==[6])
+        self.assertTrue(s.calculate("1,2,3,4")==[4,1])
