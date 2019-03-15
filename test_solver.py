27ac42f21ba0dfb385bed1b404561922c39a67b5
ramiro.vargassalas <ramiro.vargassalas@gmail.com>
2019-03-15 13:02:23 -0500

    Azul iteracion 1

--- test_solver.py
+++ test_solver.py
@@ -6,6 +6,9 @@ class TestSolver(TestCase):
     def test_calculate_iter1(self):
         s=Solver()
         self.assertTrue(s.calculate("")==[0,0])
-    def test_calculate_iter1(self):
+    def test_calculate_iter2(self):
         s=Solver()
         self.assertTrue(s.calculate("1,2,3,4")==[4,1])
+    def test_calculate_iter3(self):
+        s=Solver()
+        self.assertTrue(s.calculate("2,2,3,4,5,6,7,8")==[8,2])
